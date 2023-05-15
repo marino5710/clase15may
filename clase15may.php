1f251efea9101d8e91ce6b48841b7fafe45e0c66
marino5710 <brianmarin4@gmail.com>
2023-05-15 09:29:47 -0600

    se termino de crear las classes heredadas

---
+++ clase15may.php
@@ -0,0 +1,64 @@
+<?php
+//final
+//abstract
+//static
+
+abstract class Embarcaciones {
+    protected $eslora; 
+    public $manga;
+    public static $color = "gris"; 
+
+    public function __construct($eslora, $manga)
+    {
+        $this->eslora = $eslora; 
+        $this->manga = $manga;
+    }
+
+    final public static function getColor(){
+        echo"La embarcacion es de color " . self::$color; 
+    }
+
+    public function getInfo() : void {
+        echo "La embarcacion tiene $this->eslora eslora, y tiene una manga de: $this->manga"; 
+    }
+
+    public function getManga() : void {
+        echo "La embarcacion tiene $this->manga manga"; 
+    }
+}
+
+$embarcaciones = new Embarcaciones (65, 10); 
+
+
+class Tiburonera extends Embarcaciones{
+    public $motor;
+
+    public function __construct($eslora = 0, $manga = 0, $motor = 0)
+    {
+        $this->motor = $motor; 
+        parent::__construct($eslora, $manga);
+    }
+    
+    public function getInfo() : void {
+        echo "La embarcacion $this->eslora eslora, y tiene una manga de: $this->manga";
+        echo " y tiene un motor de: $this->motor";
+    }
+
+}
+
+
+
+$tiburonera75 = new Tiburonera(27,6); 
+$tiburonera75->getInfo();
+echo "/n"; 
+
+class Lancharapida extends Embarcaciones {
+    public function getInfo(): void
+    {
+        echo "La Lancha rapida tiene  $this->eslora eslora, y tiene una manga de: $this->manga";
+    }
+
+}
+
+$Lancharapida = new Lancharapida(25,5); 
+$Lancharapida->getInfo();
